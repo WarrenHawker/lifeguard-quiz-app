c433c79470de61d2ec5aa439bad73ef9667a3de8
WanderingMist <hawker.warren@gmail.com>
2022-10-21 03:43:10 +0100

    fixed results overlay transitions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {questionsData} from '../assets/data';
 import Question from './Question';
 import Pagination from './Pagination';
@@ -11,7 +11,7 @@ export default function Quiz(props) {
 	const [currentPage, setCurrentPage] = useState(1)
   const [score, setScore] = useState(null)
   const [showCorrectAnswers, setShowCorrectAnswers] = useState(false)
-  const [showResults, setShowResults] = useState(false)
+  const [showResults, setShowResults] = useState()
 
   useEffect(() => {
     if(props.options.mode == 'mock') {
@@ -110,15 +110,52 @@ export default function Quiz(props) {
   }
 
   const checkAnswers = () => {
-    let count = 0
+    let newScore = {
+      mode: props.options.mode,
+      totalQuestions: questions.length,
+      questionsSection1: 0,
+      questionsSection2: 0,
+      questionsSection3: 0,
+
+      totalCorrectAnswers: 0,
+      correctAnswersSection1: 0,
+      correctAnswersSection2: 0,
+      correctAnswersSection3: 0,
+    }
+
     questions.forEach((question) => {
       question.answers.forEach((answer) => {
         if(answer.isSelected && answer.isCorrect) {
-          return count++
+          switch(question.category) {
+            case 1:
+              newScore.correctAnswersSection1 +=1
+              break;
+            case 2:
+              newScore.correctAnswersSection2 +=1
+              break;
+            case 3:
+              newScore.correctAnswersSection3 +=1
+              break;
+          }
         }
       })
     })
-    setScore(count)
+
+    questions.forEach((question) => {
+        switch(question.category) {
+          case 1:
+            newScore.questionsSection1 +=1
+            break;
+          case 2:
+            newScore.questionsSection2 +=1
+            break;
+          case 3:
+            newScore.questionsSection3 +=1
+            break;
+        }
+      })
+
+    setScore(newScore)
     setShowCorrectAnswers(true)
     setShowResults(true)
   }
@@ -142,6 +179,17 @@ export default function Quiz(props) {
     setShowResults(true)
   }
 
+  const hideResults = () => {
+   setShowResults(false)
+  }
+
+  let resultsOverlayStyle
+  if(showResults == true) {
+    resultsOverlayStyle = 'results-overlay active'
+  } else {
+    resultsOverlayStyle = 'results-overlay'
+  }
+
   return (
     <>
       <section className='questions-container'>
@@ -157,7 +205,9 @@ export default function Quiz(props) {
           </div>
         }
       </section>
-      {showResults ? <Results/> : <></>}
+      <div className={resultsOverlayStyle}>
+      {showResults ? <Results score={score} showResults={showResults} hideResults={hideResults}/> : <></>}
+      </div>
     </>
   )
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,32 @@
-export default function Results() {
-  return (
-   <></>
-  )
+export default function Results(props) {
+  if(props.score) {
+    let Percentages = {
+      total: (props.score.totalCorrectAnswers / props.score.totalQuestions) * 100,
+      section1: (props.score.correctAnswersSection1 / props.score.questionsSection1) * 100,
+      section2: (props.score.correctAnswersSection2 / props.score.questionsSection2) * 100,
+      section3: (props.score.correctAnswersSection3 / props.score.questionsSection3) * 100,
+    }
+    return (
+        <div className="results-container">
+        <i onClick={props.hideResults} className="fa fa-window-close" id="results-close"></i>
+          <h1>Your results</h1>
+          <div className="results-sub-container">
+            <h4>Totals</h4>
+            <p>total questions:</p> <span>{props.score.totalQuestions}</span>
+            <p>total correct answers:</p> <span>{props.score.totalCorrectAnswers}</span>
+            <p>percentage of answers correct:</p> <span>{Percentages.total}%</span>
+          </div>
+          <div className="results-sub-container">
+          <h4>Category breakdown</h4>
+            <p>Section 1: {props.score.correctAnswersSection1} / {props.score.questionsSection1}</p> <span>{isNaN(Percentages.section1)? 0 : Percentages.section1}%</span>
+            <p>Section 2: {props.score.correctAnswersSection2} / {props.score.questionsSection2}</p> <span>{isNaN(Percentages.section2)? 0 : Percentages.section2}%</span>
+            <p>Section 3: {props.score.correctAnswersSection3} / {props.score.questionsSection3}</p> <span>{isNaN(Percentages.section3)? 0 : Percentages.section3}%</span>
+          </div>
+    
+          <h5>In the real theory paper, the pass mark is <span>80% per section.</span></h5>      
+          <h5>Please review any questions you got wrong. If you have any queries, speak to your trainer</h5>
+        </div>
+      )
+  }
+  
 }
